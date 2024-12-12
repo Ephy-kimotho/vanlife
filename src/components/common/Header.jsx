e889db667125cb64d2aa11357cc01aabfb50c008
Ephy Kimotho <ephyk48@gmail.com>
2024-12-12 21:58:12 +0300

    feat: modified navigation bar to be responsive on mobile

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,33 @@
 import { NavLink, Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaXmark } from "react-icons/fa6";
 
 function Header() {
+  const [isOpen, setIsopen] = useState(false);
   return (
-    <header className="bg-cream py-6 px-2 sm:px-8 flex justify-between items-center">
+    <header className="bg-cream py-6 px-2 sm:px-8 flex flex-wrap justify-between items-center">
       <Link to="/">
         <img src={Logo} alt="vanlife logo" className="w-32 sm:w-40" />
       </Link>
 
-      <nav className="flex gap-4 sm:gap-8  mr-2 sm:mr-8">
+      <button
+        onClick={() => setIsopen((prev) => !prev)}
+        className="text-2xl font-bold p-2 mr-1 sm:hidden"
+      >
+        {isOpen ? (
+          <FaXmark className="text-appleRed" />
+        ) : (
+          <RxHamburgerMenu className="text-night" />
+        )}
+      </button>
+
+      <nav
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } sm:flex flex-col sm:flex-row items-center gap-4 sm:gap-8 w-full sm:w-min mr-2 sm:mr-8`}
+      >
         <NavLink
           to="host"
           className={({ isActive }) =>
@@ -16,6 +35,7 @@ function Header() {
               ? "text-base sm:text-xl text-night underline font-bold"
               : "text-base sm:text-xl text-gray-200 hover:text-night hover:underline"
           }
+          onClick={() => setIsopen(false)}
         >
           Host
         </NavLink>
@@ -27,6 +47,7 @@ function Header() {
               ? "text-base sm:text-xl text-night underline font-bold"
               : "text-base sm:text-xl text-gray-200 hover:text-night hover:underline"
           }
+          onClick={() => setIsopen(false)}
         >
           About
         </NavLink>
@@ -38,6 +59,7 @@ function Header() {
               ? "text-base sm:text-xl text-night underline font-bold"
               : "text-base sm:text-xl text-gray-200 hover:text-night hover:underline"
           }
+          onClick={() => setIsopen(false)}
         >
           Vans
         </NavLink>
